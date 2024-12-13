4ea9ad99b9a7fcd93311f919e2f189ace5dd9063
hanwoosung <yiok79@naver.com>
2024-12-13 14:28:42 +0900

    테스트

---
+++ src/main/java/org/green/hckh/HckhApplication.java
@@ -0,0 +1,13 @@
+package org.green.hckh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HckhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HckhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/green/hckh/HckhApplicationTests.java
@@ -0,0 +1,13 @@
+package org.green.hckh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HckhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
