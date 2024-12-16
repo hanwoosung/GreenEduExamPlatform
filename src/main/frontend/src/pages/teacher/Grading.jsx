d6baff831fecb807b87781ce0d1b7f997bfb6fe5
unknown <yiok79@naver.com>
2024-12-17 00:01:59 +0900

    공통api 수정

--- src/main/frontend/src/pages/teacher/Grading.jsx
+++ src/main/frontend/src/pages/teacher/Grading.jsx
@@ -7,13 +7,19 @@ import GradesTable from "../../components/teacher/grading/GradesTable";
 import LectureButton from "../../components/teacher/grading/LectureButton";
 import GradingTitle from "../../components/teacher/grading/GradingTitle";
 import useFetch from "../../hooks/useFetch";
+import useApi2 from "../../hooks/useApi2";
 
 const Grading = () => {
-    const { data: lectures, loading } = useFetch("/api/v1/grading/class/teacher1");
+    const {data: lectures, loading} = useFetch("/api/v1/grading/class/teacher1");
+    const { get } = useApi2();
     const [subjects, setSubjects] = useState([]);
 
+    const [step, setStep] = useState(1);
+    const [selectedLecture, setSelectedLecture] = useState('');
+    const [selectedSubject, setSelectedSubject] = useState('');
+    const [checkedStudents, setCheckedStudents] = useState(new Set());
 
-    const grades = [ //과제에맞는 학생성적을 들고온다.c v에라잉!@#!@@#!@#@!#안들고올래
+    const grades = [
         {id: 1, name: '김철수', grade: 'A'},
         {id: 2, name: '이영희', grade: 'B+'},
         {id: 3, name: '박민수', grade: 'B'},
@@ -21,15 +27,20 @@ const Grading = () => {
         {id: 5, name: '홍길동', grade: 'C+'}
     ];
 
-    const [step, setStep] = useState(1);
-    const [selectedLecture, setSelectedLecture] = useState('');
-    const [selectedSubject, setSelectedSubject] = useState('');
-    const [checkedStudents, setCheckedStudents] = useState(new Set());
-
-    const handleLectureSelect = (lecture) => {
-        setSelectedLecture(lecture);
+    const handleLectureSelect = async (lecture) => {
+        setSelectedLecture(lecture.className);
+        try {
+            const response = await get(`/api/v1/grading/schedule/teacher1`, {
+                params: { num: lecture.id },
+            });
+            console.log("과목 리스트 응답 데이터:", response);
+            setSubjects(response);
+        } catch (err) {
+            console.error("과목 리스트를 가져오는 중 에러 발생:", err);
+        }
         setStep(2);
     };
+
 
     const handleSubjectSelect = (subject) => {
         setSelectedSubject(subject);
@@ -62,13 +73,13 @@ const Grading = () => {
             {/*강의 선택*/}
             {!loading && step === 1 && (
                 <div className="card">
-                    <GradingTitle title="강의를 선택하세요" />
+                    <GradingTitle title="강의를 선택하세요"/>
                     <div className="options">
                         {lectures?.map((lecture) => (
                             <LectureButton
                                 key={lecture.id}
                                 lecture={lecture.className}
-                                onClick={() => handleLectureSelect(lecture.className)}
+                                onClick={() => handleLectureSelect(lecture)}
                             />
                         ))}
 
@@ -79,16 +90,15 @@ const Grading = () => {
             {/*과목 선택*/}
             {step === 2 && (
                 <div className="card">
-                    <GradingTitle title={`${selectedLecture} 과목을 선택하세요`} onBack={goBack} />
+                    <GradingTitle title={`${selectedLecture} 과목을 선택하세요`} onBack={goBack}/>
                     <div className="options">
-                        {subjects[selectedLecture]?.map((subject) => (
+                        {subjects?.map(subject => (
                             <SubjectButton
-                                key={subject}
-                                subject={subject}
+                                key={subject.id}
+                                subject={subject.scheduleName}
                                 onClick={() => handleSubjectSelect(subject)}
                             />
                         ))}
-
                     </div>
                 </div>
             )}
@@ -96,10 +106,10 @@ const Grading = () => {
             {/*성적 관리*/}
             {step === 3 && (
                 <div className="card">
-                    <GradingTitle title={`${selectedSubject}의 성적 리스트`} onBack={goBack} />
+                    <GradingTitle title={`${selectedSubject.scheduleName}의 성적 리스트`} onBack={goBack}/>
                     <div className="action-buttons">
-                        <ActionButton onClick={confirmGrades} className="confirm" icon={FaCheckCircle} label="확정 여부" />
-                        <ActionButton onClick={scheduleRetest} className="retest" icon={FaRedoAlt} label="재시험" />
+                        <ActionButton onClick={confirmGrades} className="confirm" icon={FaCheckCircle} label="확정 여부"/>
+                        <ActionButton onClick={scheduleRetest} className="retest" icon={FaRedoAlt} label="재시험"/>
                     </div>
                     <GradesTable
                         grades={grades}
