fdc680f016903b96e72055bf06fc4b59c5e18acf
HSH <victoryline629@naver.com>
2024-12-20 14:25:39 +0900

    기환씨 사이드바 되게 함

--- src/main/frontend/src/components/teacher/TestSideBar.jsx
+++ src/main/frontend/src/components/teacher/TestSideBar.jsx
@@ -1,7 +1,5 @@
 import React, {useRef} from 'react';
-/*
 import "../../assets/css/teacher/test/TestSidebar.css";
-*/
 
 const TestSidebar = (props) => {
     const {questions, scrollToQuestionBox} = props;
